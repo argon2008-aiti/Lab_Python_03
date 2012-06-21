e61a3018be04948d9c193678e25d463498375b5b
argon2008-aiti <macpherson2008@yahoo.com>
2012-06-21 23:50:13 +0000

    Update master

--- Lab_03a.py
+++ Lab_03a.py
@@ -1,11 +1,1 @@
-start_Number = 1
-stop_Number  = 0
-while stop_Number < 6:
-    for number in range(1,start_Number+1):
-        print number,
 
-        start_Number += 1
-
-    stop_Number += 1
-    
-    
